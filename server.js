94c50d9f770fa19b183e5fae6ed2455507fa7808
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-06-27 19:07:21 -0600

    Add base controller and inventory route for server

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -25,9 +27,10 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"});
-})
+app.get("/",baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information

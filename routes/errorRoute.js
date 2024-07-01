d362e662fa0d0d48408a7ed2c6bbb6faab923c82
tomaseliasfontes <tomaseliasfontes@gmail.com>
2024-07-01 14:10:48 -0600

    adding error route and test

--- routes/errorRoute.js
+++ routes/errorRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Util = require('../utilities/index');
+const utilities = require('../utilities');
 
-router.get('/trigger-error', Util.handleErrors(async (req, res, next) => {
-  throw new Error('This is an intentional error for testing purposes');
+router.get('/trigger-error', utilities.handleErrors(async (req, res, next) => {
+  const error = new Error('This is an intentional error for testing purposes');
+  error.status = 500;
+  throw error;
 }));
 
 module.exports = router;
